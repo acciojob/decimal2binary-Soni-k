7ff5a44ce4212999243608e070e1792ab401d202
Soni kumari <126183283+Soni-k@users.noreply.github.com>
2023-12-12 19:55:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+  // Check if the input is not a valid number
+  if (isNaN(num) || num < 0 || !Number.isInteger(num)) {
+    return "Invalid integer.";
+  }
+
+  // Handle the special case for 0
+  if (num === 0) {
+    return "0";
+  }
+
+  let binary = "";
+  while (num > 0) {
+    // Append the least significant bit (LSB) of num to binary
+    binary = (num % 2) + binary;
+    // Right-shift num by 1 to process the next bit
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
